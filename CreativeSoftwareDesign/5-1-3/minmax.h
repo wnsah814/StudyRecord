edf6a0d4b6ed5a55e70884affa179a922e1e5853
성준모 <sjmskm@gmail.com>
2021-11-14 20:00:48 +0900

    Delete CreativeSoftwareDesign directory

--- CreativeSoftwareDesign/5-1-3/minmax.h
+++
@@ -1,6 +0,0 @@
-#ifndef MINMAX_H_
-#define MINMAX_H_
-
-void getMinMax(int* arr, int len, int& min, int& max);
-
-#endif
--- CreativeSoftwareDesign/8-1-1/number.cpp
+++
@@ -1,9 +0,0 @@
-#include "number.h"
-
-int Square::getSquare() {
-    return _num * _num;
-}
-
-int Cube::getCube() {
-    return _num * _num * _num;
-}
